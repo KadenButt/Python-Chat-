4ee726ff0f0018fb27e1135df0a44ad6257b5867
Kaden Butt <kadenjbutt@gmail.com>
2022-07-13 22:41:19 +0100

    GUI is working, networking code is not coded in

--- client/client_GUI.py
+++ client/client_GUI.py
@@ -4,23 +4,30 @@ import socket
 import multiprocessing
 
 root = Tk()
-root.title("Test chat")
-root.geometry("400x400")
+root.title("Chat Screen")
+root.geometry("1000x800")
+
+chat_list = []
+
 
 class Elder:
     def __init__(self, master):
-        myFrame = Frame(master)
-        myFrame.pack()
-        self.hello(master)
-        self.myButton = Button(master, text="Click me", command=self.clicker)
-        self.myButton.pack(pady=20)
+        screen = Label(master, bg="black", fg="green", text="", anchor=SW)
+        msg_input = Entry(master)
+        send_message = Button(master, text="Send")
 
-    def clicker(self):
-        print("Look at you")
+        screen.place(x=0, y=0, width=1000, height=750)
+        msg_input.place(x=0, y=750, width=950, height=50)
+        send_message.place(x=950, y=750, width=50, height=50)
 
-    def hello(self, master):
-        print("hello")
-        master.after(0,lambda: self.hello(master,))
+        self.hello(master,screen)
+
+    def hello(self, master, screen):
+        chat_list.append("test")
+        if len(chat_list) > 42:
+                chat_list.pop(1)
+        screen.config(text=" ".join(chat_list))
+        master.after(500,lambda: self.hello(master,screen))
 
 e = Elder(root)
 root.mainloop()
