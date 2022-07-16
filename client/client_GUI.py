07d23a2be2b12ecc61718269c64ac6bc4e1add25
Kaden Butt <kadenjbutt@gmail.com>
2022-07-16 16:33:31 +0100

    The first working version

--- client/client_GUI.py
+++ client/client_GUI.py
@@ -1,7 +1,5 @@
 from tkinter import *
-from functions import *
 import socket
-from threading import Thread
 
 root = Tk()
 root.title("Chat Screen")
@@ -23,36 +21,43 @@ chat_list = []
 
 class Main_Screen:
     def __init__(self, master):
+        ##displays screen widgets
         screen = Label(master, bg="black", fg="green", text="", anchor=SW)
         msg_input = Entry(master)
-        send_message = Button(master, text="Send",command=lambda: self.send_msg(msg_input,))
+        send_message = Button(master, text="Send",command=lambda: self.input_button_press(msg_input,))
 
         screen.place(x=0, y=0, width=1000, height=750)
         msg_input.place(x=0, y=750, width=950, height=50)
         send_message.place(x=950, y=750, width=50, height=50)
-        t1 = Thread(target=self.display_receviced_msg, args=(master,screen))
-        t1.start()
+        self.display_receviced_msg(master,screen)
 
-    def send_msg(self, input_widget):
+    def input_button_press(self, input_widget):
         msg = input_widget.get()
+        self.send_msg(msg)
+        input_widget.delete(0, END)
+
+
+##sends msg to server
+    def send_msg(self, msg):
+        msg += "\n"
         message = msg.encode(FORMAT)
         msg_length = len(message)
         send_length = str(msg_length).encode(FORMAT)
         send_length += b" " * (HEADER - len(send_length))
         client.send(send_length)
         client.send(message)
-        input_widget.delete(0, END)
 
+##dispays msg from server to label called "screen"
     def display_receviced_msg(self, master, screen):
+        self.send_msg("[INCOMEING MESSAGE]")
         chat_list.append(client.recv(20480).decode(FORMAT)+ "\n")
         if len(chat_list) > 42:
                 chat_list.pop(1)
         screen.config(text=" ".join(chat_list))
-        master.after(2000,lambda: self.test(master,screen))
+        ##calls function start_threading after 2 seeconds
+        master.after(2000,lambda: self.display_receviced_msg(master,screen))
 
-    def test(self,master,screen):
-        t1 = Thread(target=self.display_receviced_msg, args=(master,screen))
-        t1.start()
+
 
 
 e = Main_Screen(root)
